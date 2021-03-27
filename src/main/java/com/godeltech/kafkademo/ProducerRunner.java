0223ec591e3b76b54f7520f66df07263fb027b46
Ihar Novik <i.novik@godeltech.com>
2021-03-27 13:46:35 +0300

    Code cleanup.

--- src/main/java/com/godeltech/kafkademo/ProducerRunner.java
+++ src/main/java/com/godeltech/kafkademo/ProducerRunner.java
@@ -1,41 +1,23 @@
 package com.godeltech.kafkademo;
 
-import com.github.javafaker.Faker;
 import com.godeltech.kafkademo.service.CustomerProducer;
-import godel.demo.Customer;
-import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@CommonsLog(topic = "ProducerRunner")
 public class ProducerRunner implements ApplicationRunner {
 
 	private final CustomerProducer customerProducer;
 
 	@Override
 	public void run(ApplicationArguments args) {
-
-		Stream.iterate(1, i -> i + 1)
-			.limit(300)
-			.map(this::createNewCustomer)
-			.forEach(this::produceNewCustomer);
-	}
-
-	private Customer createNewCustomer(int customerId) {
-		val fakeName = new Faker().name();
-
-		return Customer.newBuilder()
-			.setCustomerId(customerId)
-			.setFirstName(fakeName.firstName())
-			.setSecondName(fakeName.lastName())
-			.build();
-	}
-
-	private void produceNewCustomer(Customer customer) {
-		customerProducer.sendMessage(customer);
+		log.info("Starting to produce customer");
+		customerProducer.produceNewCustomers(300);
+		log.info("Finish to produce customer");
 	}
 }
--- src/main/java/com/godeltech/kafkademo/service/CustomerProducer.java
+++ src/main/java/com/godeltech/kafkademo/service/CustomerProducer.java
@@ -1,7 +1,9 @@
 package com.godeltech.kafkademo.service;
 
+import com.github.javafaker.Faker;
 import com.godeltech.kafkademo.configuration.KafkaConfiguration;
 import godel.demo.Customer;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.apachecommons.CommonsLog;
 import lombok.val;
@@ -16,8 +18,27 @@ public class CustomerProducer {
 
 	private final KafkaConfiguration kafkaConfiguration;
 	private final KafkaProducer<String, Customer> kafkaProducer;
+	
+	public void produceNewCustomers(int numberOfCustomers){
+		Stream.iterate(1, i -> i + 1)
+			.limit(numberOfCustomers)
+			.map(this::createNewCustomer)
+			.forEach(this::sendMessage);
 
-	public void sendMessage(Customer customer) {
+		kafkaProducer.close();
+	}
+
+	private Customer createNewCustomer(int customerId) {
+		val fakeName = new Faker().name();
+
+		return Customer.newBuilder()
+			.setCustomerId(customerId)
+			.setFirstName(fakeName.firstName())
+			.setSecondName(fakeName.lastName())
+			.build();
+	}
+	
+	private void sendMessage(Customer customer) {
 		val record = new ProducerRecord<>(kafkaConfiguration.getCustomerInputTopic(),
 			String.valueOf(customer.getCustomerId()), customer);
 
